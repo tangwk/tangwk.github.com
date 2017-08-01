ea70d3e0e2d2871ec38b881cd05544e06f150ea4
tangwk <tangwk@users.noreply.github.com>
2017-08-01 19:07:49 +0800

    使用ASP.Net MVC 5搭建一个三层架构的项目，可以录入员工基本信息及教育信息。 员工基本信息包含：姓名、工号、性别、民族、单位、部门、出生日期、国籍、手机号码 教育信息包含：毕业学校、入学日期、毕业日期、所得学历、学制

---
+++ EmployerManger/EmployerManger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployerManger.Startup))]
+namespace EmployerManger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
