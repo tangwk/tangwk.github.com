b43efbaf5687011bc5e0b385d26b4d76b239c516
tangwk <tangwk@users.noreply.github.com>
2017-08-01 19:42:21 +0800

    jQuery API
    自己实现的jQuery AOI

---
+++ Jquery api/Jquery api/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 检查程序集特性的值
+<Assembly: AssemblyTitle("Jquery_api")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Jquery_api")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+<Assembly: Guid("f7d2c77d-9f35-4fab-bb1e-87facad4ebef")> 
+
+' 程序集的版本信息由下列四个值组成:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' 可以指定所有值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用 "*":
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
